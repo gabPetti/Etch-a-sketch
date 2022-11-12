8ecca420a754b908305cc8dd86a726c51ae3a1bf
gabPetti <gabpettidev@gmail.com>
2022-11-12 17:17:00 -0300

    Style canvas with display grid

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function canvasSize() {
     canvas = document.getElementById('canvas');
+    canvas.innerHTML = "";
     canvasX = parseInt(document.querySelector("#canvas-x").value);
     canvasY = parseInt(document.querySelector("#canvas-y").value);
 
@@ -8,6 +9,9 @@ function canvasSize() {
     for (let i = 0; i < num; i++) {
         canvas.innerHTML = canvas.innerHTML + "<div></div>";
     }
+
+    canvasGridTemplateText = " 15px".repeat(canvasX);
+    canvas.style.cssText = 'grid-template-columns:' + canvasGridTemplateText + ';';
 }
 
 canvasSize();
