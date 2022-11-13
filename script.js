35c33ff47afecf7557a929d3d8ca9cba48e4888c
gabPetti <gabpettidev@gmail.com>
2022-11-13 17:44:26 -0300

    Optimize canvas with display grid

--- script.js
+++ script.js
@@ -6,36 +6,6 @@ slider.oninput = function() {
     sliderOutput.innerHTML = this.value + ' x ' + this.value;
 }
 
-function canvasSize() {
-    var canvas = document.getElementById('canvas');
-    canvas.innerHTML = "";
-
-    var canvasSize = parseInt(document.querySelector("#canvas-size").value);
-    var num = canvasSize * canvasSize;
-    for (let i = 0; i < num; i++) {
-        canvas.innerHTML = canvas.innerHTML + '<div onmouseover="coloring(this)"></div>';
-    }
-
-    canvas.style.cssText = 'width: ' + (canvasSize * 15).toString() + 'px;';
-}
-
-canvasSize();
-
-function coloring(event) {
-    event.style.backgroundColor = document.querySelector(".color").value;
-}
-
-function changePenColor() {
-    pen = document.querySelector(".color").value;
-}
-
-function toggleGridlines() {
-    var toggle = document.querySelector(".toggle");
-    var gridlines = document.getElementById("canvas");
-    toggle.classList.toggle("on");
-    gridlines.classList.toggle("on");
-}
-
 function toggleAside() {
     var aside = document.querySelector("aside");
     var navbar = document.querySelector("nav")
@@ -46,4 +16,36 @@ function toggleAside() {
     closeAside.classList.toggle("on");
     main.classList.toggle("on");
     navbar.classList.toggle("on");
+}
+
+function canvasSize() {
+    var canvas = document.getElementById('canvas');
+    canvas.innerHTML = "";
+
+    var canvasSize = parseInt(document.querySelector("#canvas-size").value);
+    canvas.style.cssText = 'grid-template-columns:' + ' auto'.repeat(canvasSize) + ';';
+
+    var divNum = canvasSize * canvasSize;
+    for (let i = 0; i < divNum; i++) {
+        canvas.innerHTML = canvas.innerHTML + '<div onmouseover="coloring(this)"></div>';
+    }
+}
+
+canvasSize();
+
+/* Aside functions --------------------------------------------*/
+
+function changePenColor() {
+    pen = document.querySelector(".color").value;
+}
+
+function coloring(event) {
+    event.style.backgroundColor = document.querySelector(".color").value;
+}
+
+function toggleGridlines() {
+    var toggle = document.querySelector(".toggle");
+    var gridlines = document.getElementById("canvas");
+    toggle.classList.toggle("on");
+    gridlines.classList.toggle("on");
 }
